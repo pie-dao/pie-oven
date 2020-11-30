a0824690827c6497c46da985072d282263196219
Mick de Graaf <mick@mickdegraaf.nl>
2020-11-30 15:03:59 +0100

    Swap ETH -> WETH

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,10 @@
-import { task } from "hardhat/config";
+require('dotenv').config()
+
+import { task, HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 
-require('dotenv').config()
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY || "";
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "";
@@ -110,7 +111,7 @@ task("bake", "Generate call data for bake function")
 // This object can have the following optional entries:
 // defaultNetwork, networks, solc, and paths.
 // Go to https://buidler.dev/config/ to learn more
-export default {
+const config: HardhatUserConfig = {
   // This is a sample solc configuration that specifies which version of solc to use
   solidity: {
     compilers: [
@@ -152,3 +153,5 @@ export default {
     apiKey: ETHERSCAN_API
   }
 };
+
+export default config;
