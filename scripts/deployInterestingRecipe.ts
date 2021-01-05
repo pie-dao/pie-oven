89e85e1caeb26907458665dbaaac088da6705c28
Evert Kors <evert@hatching.io>
2021-01-05 22:38:02 +0100

    update deploy script

--- scripts/deployInterestingRecipe.ts
+++ scripts/deployInterestingRecipe.ts
@@ -3,18 +3,18 @@ import {ethers} from "hardhat";
 import { InterestingRecipe } from "../typechain/InterestingRecipe";
 
 async function recipePublish() {
-    const [account] = await ethers.getSigners()
     const Recipe = await ethers.getContractFactory("InterestingRecipe");
-    const recipe = await Recipe.deploy("0x5FbDB2315678afecb367f032d93F642f64180aa3") as InterestingRecipe;
-    await recipe.deployed();
-    //const recipe = await ethers.getContractAt("InterestingRecipe", "0x0bBdf22CF30f91D7867140d0aE26466d8FC90051")
+    const lendingRegistry = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    const pie = "0x17525e4f4af59fbc29551bc4ece6ab60ed49ce31"
 
+    const recipe = await Recipe.deploy(lendingRegistry) as InterestingRecipe;
+    await recipe.deployed();
     console.log("Recipe deployed to:", recipe.address);
 
-    const amount = await recipe.callStatic.calcToPie("0xb8d379c83A10b073565bD486Ea87147E7117B025", parseEther("3"))
+    const amount = await recipe.callStatic.calcToPie(pie, parseEther("3"))
     console.log("Wei to join", amount.toString())
 
-    const tx = await recipe.toPie("0xb8d379c83A10b073565bD486Ea87147E7117B025", parseEther("3"), {value: parseEther("1")})
+    const tx = await recipe.toPie(pie, parseEther("3"), {value: parseEther("1")})
     const receipt =  await tx.wait();
     console.log(receipt)
   }
